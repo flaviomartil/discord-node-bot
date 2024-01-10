c6670bf0d4dd9f15da9445f29e7d59163156bfd9
flaviomartil <flaviomartil5@gmail.com>
2024-01-10 11:56:38 -0300

    Version node + english variables.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 require('dotenv/config');
-const { Client, IntentsBitField, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, EmbedBuilder,
-  ButtonBuilder,
-  ButtonStyle
-} = require('discord.js');
+const { Client, IntentsBitField, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { CommandHandler } = require('djs-commander');
 const path = require('path');
 
@@ -17,168 +14,157 @@ new CommandHandler({
 });
 
 client.on("ready", async () => {
-  console.log("O bot carregou com sucesso.");
+  console.log("The bot loaded successfully.");
   askSet();
   channelSuggestion();
 });
 
 client.on("interactionCreate", (interaction) => {
-  modalSet(interaction);
-  modalSuggestion(interaction);
+  if (interaction.customId === "suggestion-modal") {
+    makeModalSuggestion(interaction);
+  } else if  (interaction.customId === "request-set-modal") {
+    modalSet(interaction);
+  } else if (interaction.customId && interaction.customId.startsWith("suggestion-modal-")) {
+    submitModalSuggestion(interaction);
+  } else if (interaction.customId && interaction.customId.startsWith("requestSetModal-")) {
+    submitModalSet(interaction);
+  }
 });
 
-function modalSuggestion(interaction) {
-  if (interaction.customId === "sugestao-modal") {
-    const modal = new ModalBuilder()
-        .setCustomId(`sugestao-modal-${interaction.user.id}`)
-        .setTitle('Faça pedido do set')
-    ;
+function makeModalSuggestion(interaction) {
+  const userId = interaction.user.id;
+  const modal = new ModalBuilder()
+      .setCustomId(`suggestion-modal-${userId}`)
+      .setTitle('Faça pedido do set');
 
-    const nomeNaCidade = new TextInputBuilder()
-        .setCustomId('nomeNaCidade')
-        .setLabel("NOME NA CIDADE?")
-        .setStyle(TextInputStyle.Short)
-        .setRequired(true);
-    const sugestao = new TextInputBuilder()
-        .setCustomId('sugestao')
-        .setLabel("INSIRA SUA SUGESTÂO")
-        .setStyle(TextInputStyle.Paragraph)
-        .setRequired(true);
+  const cityName = new TextInputBuilder()
+      .setCustomId('cityName')
+      .setLabel("NOME NA CIDADE?")
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+  const suggestion = new TextInputBuilder()
+      .setCustomId('suggestion')
+      .setLabel("INSIRA SUA SUGESTÂO")
+      .setStyle(TextInputStyle.Paragraph)
+      .setRequired(true);
 
+  const firstActionRow = new ActionRowBuilder().addComponents(cityName);
+  const secondActionRow = new ActionRowBuilder().addComponents(suggestion);
+  modal.addComponents(firstActionRow, secondActionRow);
 
-    const firstActionRow = new ActionRowBuilder().addComponents(nomeNaCidade);
-    const secondActionRow = new ActionRowBuilder().addComponents(sugestao);
-    modal.addComponents(firstActionRow, secondActionRow);
+  interaction.showModal(modal);
+}
 
-    interaction.showModal(modal);
-    return;
-  }
+async function submitModalSuggestion(interaction) {
+  const userId = interaction.user.id;
+  const userMention = interaction.user.toString();
+  const filter = (interaction) => interaction.customId === `suggestion-modal-${userId}`;
 
-  if (interaction.customId.startsWith("sugestao-modal-")) {
-    const filter = (interaction) => interaction.customId === `sugestao-modal-${interaction.user.id}`;
-    interaction
-        .awaitModalSubmit({filter, time: 30_000})
-        .then((modalInteraction) => {
-          const nomeNaCidadeValue = modalInteraction.fields.getTextInputValue('nomeNaCidade');
-          const sugestaoValue = modalInteraction.fields.getTextInputValue('sugestao');
-          let embed = new EmbedBuilder()
-              .setTitle('Nova sugestão !')
-              .setDescription(`
-        Nome na cidade: ${nomeNaCidadeValue}
-        Sugestão: ${sugestaoValue}
-    `).setColor(0x7289DA);
-
-          const channel = client.channels.cache.get('1194621112764604466')
-          channel.send({embeds: [embed]})
-
-          client.users.fetch('997122698905919579', false).then((user) => {
-            user.send({ embeds: [embed] });
-          });
-
-          client.users.fetch('800849352112734230', false).then((user) => {
-            user.send({ embeds: [embed] });
-          });
-
-        }).catch((err) => {
-      console.log('Error' + err);
-    })
-    interaction.reply({
+  try {
+    const modalInteraction = await interaction.awaitModalSubmit({filter, time: 120_000});
+    const cityNameValue = modalInteraction.fields.getTextInputValue('cityName');
+    const suggestionValue = modalInteraction.fields.getTextInputValue('suggestion');
+    const embed = createEmbed('Nova sugestão!', `Nome na cidade: ${cityNameValue}\nSugestão: ${suggestionValue}`, userMention);
+    sendMessageToChannels(embed, ['997122698905919579', '800849352112734230'], '1194621112764604466');
+    modalInteraction.reply({
       ephemeral: true,
       content: `Sua sugestão foi enviada com sucesso, caso seja adicionada te avisaremos.`,
     });
+  } catch (err) {
+    console.log('Error', err);
   }
 }
+
 function modalSet(interaction) {
-  if (interaction.customId === "pedir-set-modal") {
-    const modal = new ModalBuilder()
-        .setCustomId(`pedirSetModal-${interaction.user.id}`)
-        .setTitle('Faça pedido do set')
-    ;
+  const userId = interaction.user.id;
+  const modal = new ModalBuilder()
+      .setCustomId(`requestSetModal-${userId}`)
+      .setTitle('Faça pedido do set');
 
-    const nomeNaCidade = new TextInputBuilder()
-        .setCustomId('nomeNaCidade')
-        .setLabel("NOME NA CIDADE?")
-        .setStyle(TextInputStyle.Short)
-        .setRequired(true);
-    const passaporte = new TextInputBuilder()
-        .setCustomId('passaporte')
-        .setLabel("INSIRA SEU PASSAPORTE")
-        .setStyle(TextInputStyle.Short)
-        .setRequired(true);
-    const telCidade = new TextInputBuilder()
-        .setCustomId('telCidade')
-        .setLabel("TELEFONE NA CIDADE")
-        .setStyle(TextInputStyle.Short)
-        .setRequired(true);
-    const nomeRecrutador = new TextInputBuilder()
-        .setCustomId('nomeRecrutador')
-        .setLabel("NOME DO RECRUTADOR")
-        .setStyle(TextInputStyle.Short)
-        .setRequired(true);
+  const cityName = new TextInputBuilder()
+      .setCustomId('cityName')
+      .setLabel("NOME NA CIDADE?")
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+  const passport = new TextInputBuilder()
+      .setCustomId('passport')
+      .setLabel("INSIRA SEU PASSAPORTE")
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+  const cityPhone = new TextInputBuilder()
+      .setCustomId('cityPhone')
+      .setLabel("TELEFONE NA CIDADE")
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+  const recruiterName = new TextInputBuilder()
+      .setCustomId('recruiterName')
+      .setLabel("NOME DO RECRUTADOR")
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
 
-    const firstActionRow = new ActionRowBuilder().addComponents(nomeNaCidade);
-    const secondActionRow = new ActionRowBuilder().addComponents(passaporte);
-    const thirdActionRow = new ActionRowBuilder().addComponents(telCidade);
-    const fourActionRow = new ActionRowBuilder().addComponents(nomeRecrutador);
-    modal.addComponents(firstActionRow, secondActionRow,thirdActionRow, fourActionRow);
+  const firstActionRow = new ActionRowBuilder().addComponents(cityName);
+  const secondActionRow = new ActionRowBuilder().addComponents(passport);
+  const thirdActionRow = new ActionRowBuilder().addComponents(cityPhone);
+  const fourActionRow = new ActionRowBuilder().addComponents(recruiterName);
+  modal.addComponents(firstActionRow, secondActionRow, thirdActionRow, fourActionRow);
 
-    interaction.showModal(modal);
-    return;
-  }
+  interaction.showModal(modal);
+}
 
-  if (interaction.customId.startsWith("pedirSetModal-")) {
-    const filter = (interaction) => interaction.customId === `pedirSetModal-${interaction.user.id}`;
-    interaction
-        .awaitModalSubmit({filter, time: 30_000})
-        .then((modalInteraction) => {
-          const nomeNaCidadeValue = modalInteraction.fields.getTextInputValue('nomeNaCidade');
-          const passaporteValue = modalInteraction.fields.getTextInputValue('passaporte');
-          const telCidadeValue = modalInteraction.fields.getTextInputValue('telCidade');
-          const nomeRecrutadorValue = modalInteraction.fields.getTextInputValue('nomeRecrutador');
-          let embed = new EmbedBuilder()
-              .setTitle('Pedindo set !')
-              .setDescription(`
-        Nome na cidade: ${nomeNaCidadeValue}
-        Passaporte: ${passaporteValue}
-        Telefone na cidade: ${telCidadeValue}
-        Nome do recrutador: ${nomeRecrutadorValue}
-    `).setColor(0x7289DA);
-          const channel = client.channels.cache.get('1194613131763253319')
-          channel.send({embeds: [embed]})
+async function submitModalSet(interaction) {
+  const userId = interaction.user.id;
+  const userMention = interaction.user.toString();
+  const filter = (interaction) => interaction.customId === `requestSetModal-${userId}`;
 
-          client.users.fetch('997122698905919579', false).then((user) => {
-            user.send({ embeds: [embed] });
-          });
-
-          client.users.fetch('800849352112734230', false).then((user) => {
-            user.send({ embeds: [embed] });
-          });
-
-        }).catch((err) => {
-      console.log('Error' + err);
-    })
-    interaction.reply({
+  try {
+    const modalInteraction = await interaction.awaitModalSubmit({filter, time: 120_000});
+    const cityNameValue = modalInteraction.fields.getTextInputValue('cityName');
+    const passportValue = modalInteraction.fields.getTextInputValue('passport');
+    const cityPhoneValue = modalInteraction.fields.getTextInputValue('cityPhone');
+    const recruiterNameValue = modalInteraction.fields.getTextInputValue('recruiterName');
+    const embed = createEmbed('Pedindo set !', `Nome na cidade: ${cityNameValue}\nPassaporte: ${passportValue}\nTelefone na cidade: ${cityPhoneValue}\nNome do recrutador: ${recruiterNameValue}`, userMention);
+    sendMessageToChannels(embed, ['997122698905919579', '800849352112734230'], '1194613131763253319');
+    modalInteraction.reply({
       ephemeral: true,
       content: `Pedido de set solicitado com sucesso, aguarde alguém da moderação responder.`,
     });
+  } catch (err) {
+    console.log('Error', err);
   }
 }
+
+function createEmbed(title, description, userMention) {
+  let descriptionEmbed = `${description}`
+  if (userMention) {
+    descriptionEmbed = `${description}\nDiscord: ${userMention}`
+  }
+  return new EmbedBuilder()
+      .setTitle(title)
+      .setDescription(descriptionEmbed)
+      .setColor(0x7289DA);
+}
+
+function sendMessageToChannels(embed, userIDs, channelID) {
+  const channel = client.channels.cache.get(channelID);
+  channel.send({embeds: [embed]});
+
+  userIDs.forEach(async (userID) => {
+    const user = await client.users.fetch(userID, false);
+    user.send({embeds: [embed]});
+  });
+}
+
 async function askSet() {
   const channel = await client.channels.fetch("1194358308002345091");
   const messages = await channel.messages.fetch();
 
-  let embed = new EmbedBuilder()
-      .setTitle('Seja bem-vindo(a) faça a solicitação do seu set aqui, basta interagir com o botão abaixo e esperar algum superior aprovar!')
-      .setDescription(`
-      Tropa da romanov
-    `).setColor(0x7289DA);
-
+  let embed = createEmbed('Seja bem-vindo(a) faça a solicitação do seu set aqui, basta interagir com o botão abaixo e esperar algum superior aprovar!', 'Tropa da Romanov');
   if (messages.size === 0) {
     channel.send({
       components: [
         new ActionRowBuilder().addComponents(
             new ButtonBuilder()
-                .setCustomId("pedir-set-modal")
+                .setCustomId("request-set-modal")
                 .setLabel("Pedir Set")
                 .setStyle(ButtonStyle.Secondary)
                 .setEmoji({ name: "📄" })
@@ -188,22 +174,18 @@ async function askSet() {
     });
   }
 }
+
 async function channelSuggestion() {
   const channel = await client.channels.fetch("1194619056695803934");
   const messages = await channel.messages.fetch();
 
-  let embed = new EmbedBuilder()
-      .setTitle('Seja bem-vindo(a) deixe a sua sugestão aqui!')
-      .setDescription(`
-      Se for uma boa sugestão, iremos implementar, todos são convidados a dar idéia pra nossa tropa continuar crescendo
-    `).setColor(0x7289DA);
-
+  let embed = createEmbed('Seja bem-vindo(a) deixe a sua sugestão aqui!', 'Se for uma boa sugestão, iremos implementar, todos são convidados a dar idéia pra nossa tropa continuar crescendo.');
   if (messages.size === 0) {
     channel.send({
       components: [
         new ActionRowBuilder().addComponents(
             new ButtonBuilder()
-                .setCustomId("sugestao-modal")
+                .setCustomId("suggestion-modal")
                 .setLabel("Sugerir")
                 .setStyle(ButtonStyle.Secondary)
                 .setEmoji({name: "💡"})
@@ -213,4 +195,5 @@ async function channelSuggestion() {
     });
   }
 }
+
 client.login(process.env.TOKEN);
