81b7f22d7e1a5db5ad32b4d32a6d9cc979b5da88
flaviomartil <flaviomartil5@gmail.com>
2024-01-15 12:53:51 -0300

    list members remove ex member and add ex member

--- slash-commands/listarmembros.js
+++ slash-commands/listarmembros.js
@@ -14,21 +14,30 @@ module.exports = {
         const guild = await interaction.guild.members.fetch();
         const role = interaction.guild.roles.cache.find(role => role.id !== "1196479865575178340")
         const totalAway = role.members.map(m => m.user.toString())
-        const afkMembers = totalAway.join('\n ');
+        const members = totalAway.join('\n ');
 
-        if (!afkMembers) {
+        const maxLength = 1024;
+
+        const parts = [];
+        for (let i = 0; i < members.length; i += maxLength) {
+          parts.push(members.substring(i, i + maxLength));
+        }
+
+        if (!members) {
           interaction.reply('Não temos membros no momento');
           return;
         }
 
-        const embed = new EmbedBuilder()
-            .setTitle('Listando todos os membros romanov')
-            .setColor(0xFFD700)
-            .addFields({ name: 'Membros:', value: afkMembers, inline: false }
-            );
+        parts.forEach((part, index) => {
+          const embed = new EmbedBuilder()
+              .setTitle('Listando todos os membros romanov')
+              .setColor(0xFFD700)
+              .addFields({name: 'Membros:', value: part, inline: false}
+              );
 
-        interaction.reply('Listando:');
-          interaction.channel.send({ embeds: [embed] });
+          interaction.reply('Listando:');
+          interaction.channel.send({embeds: [embed]});
+        });
       } else {
         interaction.reply('Esse comando só pode ser usado no servidor');
         return;
