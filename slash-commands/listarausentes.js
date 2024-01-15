e414e9e2c0851f068d0c3eeafd4e8848d8fa72a2
flaviomartil <flaviomartil5@gmail.com>
2024-01-15 10:14:30 -0300

    List afkmembers and week farm

--- slash-commands/listarausentes.js
+++ slash-commands/listarausentes.js
@@ -2,6 +2,7 @@ const {EmbedBuilder} = require("discord.js");
 const { PermissionsBitField } = require('discord.js');
 module.exports = {
   run: async ({ interaction }) => {
+    try {
     const member = interaction.member;
     const guild = await interaction.guild.members.fetch();
     const role = interaction.guild.roles.cache.find(role => role.name === "⭐")
@@ -16,17 +17,28 @@ module.exports = {
       }
     }
 
+    if (!afkMembers) {
+      interaction.reply('Não temos membros com o cargo de ausentes por enquanto, para definir coloque o cargo ⭐');
+      return;
+    }
+
     const embed = new EmbedBuilder()
         .setTitle('Esses são os membros ausentes')
         .setColor(0xFFD700)
         .addFields({ name: 'Usuários ausentes', value: afkMembers, inline: false }
         );
 
+
     interaction.reply('Listando:');
     if (interaction.channel) {
       interaction.channel.send({ embeds: [embed] });
     } else {
       interaction.user.send({embeds: [embed]});
+    }
+
+    } catch (err) {
+      console.error('Erro ao listar ausentes', err);
+      return null;
     }
   },
 
